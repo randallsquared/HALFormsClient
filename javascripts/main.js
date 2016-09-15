8cf2ef710d97e6a6b6ac84f3a397a83062858c82
Randall Randall <randall@randallsquared.com>
2016-09-15 15:34:36 -0400

    cleanup later

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,7 +16,7 @@ let gethal = (doc) => {
   let linkObjects = hal.then(doc => {
     let links = [];
     for (let key in doc._links) {
-      console.log('link', key.substring(0, 4);
+      console.log('link', key.substring(0, 4));
       if (key.substring(0, 3) === "http") {
         console.log("was http...");
         links.push({ key: key, meta: doc._links[key] });
