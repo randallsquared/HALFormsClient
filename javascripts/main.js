0d91c4fd5c55f5de8ea110572a57d3b45bac8e0f
Randall Randall <randall@randallsquared.com>
2016-09-15 14:19:08 -0400

    cleanup later

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,27 @@
-$(document).ready(function () {
-  var startup = function () {
-    console.log('startup')
-  };
+'use strict';
 
-  startup();
-});
+let task_root = 'http://rwcbook08.herokuapp.com/task/';
+
+
+let startup =  () => {
+  console.log('startup');
+  let result = Promise.resolve($.get(task_root));
+
+  result.then(checking => console.log(checking));
+};
+
+/*
+- get URL
+  - CORS
+- receive HAL
+- parse HAL
+- look for _links that start 'http:'
+  - for each such link that returns a HAL-FORMS
+    - print a form on the page which can be used to satisfy the HAL-FORMS
+      - form has to be AJAXy and the result of that can stqart over at "receive HAL"
+*/
+
+
+
+
+$(document).ready(() => startup());
